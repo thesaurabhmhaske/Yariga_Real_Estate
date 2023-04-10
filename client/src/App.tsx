8b6622d418e2e2f5a30db3a641e44a8b58cc2b2e
Saurabh Mhaske <mhaskesaurabh024@gmail.com>
2023-04-10 20:25:27 +0530

    cummmmmming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,7 +61,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://yariga-3b8v.onrender.com/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -132,7 +132,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://yariga-3b8v.onrender.com/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
